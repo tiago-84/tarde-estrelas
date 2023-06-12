56078d7a79cc44c98ffe455eff2bc2ca278e5edb
tiago <sawadeekrap1@gmail.com>
2023-06-12 18:40:49 -0300

    first commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="container mx-auto p-7 bg-zinc-400">
+    </div>
+        <div class="">
+            <div class="flex p-4 bg-slate-500">
+                    <div class=" font-bold my-3">
+                    <h1 class="text-4xl font-bold sm:text-7xl">Concorra uma experiência unica com seus ídolos!</h1>
+                    <p class="texto-xl max-w-prose">Você tem a chance unica de concorrer a uma tarde inesquecível com as estrelas que sempre adimirou.
+                    Imagine-se ao lado  dos seus idolos, desfrutando de momentos especiais e criando memórias duradouras.</p>
+                    <button>Quero saber mais</button>
+                    <button>Quero participar</button>
+                    <p>Conheça os ultimos vencedores</p>
+                </div>
+
+                <div class="flex bg-slate-500">
+                    <div>
+                        <h1 class="text-3xl font-bold my-3">Concorra uma experiência unica com seus ídolos!</h1>
+                        <p class="texto-xl max-w-prose">Você tem a chance unica de concorrer a uma tarde inesquecível com as estrelas que sempre adimirou.
+                        Imagine-se ao lado  dos seus idolos, desfrutando de momentos especiais e criando memórias duradouras.</p>
+                        <button>Quero saber mais</button>
+                        <button>Quero participar</button>
+                        <p>Conheça os ultimos vencedores</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/tests.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+
+    <div class="container mx-auto p-7 bg-zinc-400">       
+        <div class="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
+            <div class="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
+            <div class="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
+                <h1 class="text-4xl font-bold tracking-tight text-black sm:text-5xl"><span class="text-yellow-500">Concorra</span> uma experiência única com seus <span class="text-yellow-500">ídolos!</span></h1>
+                <p class="relative mt-6 text-lg leading-8 font-bold text-black sm:max-w-md lg:max-w-none mb-3">Você tem a chance única de concorrer a uma tarde inesquecível com as estrelas que sempre admirou. Imagine-se ao lado dos seus ídolos, desfrutando de momentos especiais e criando memórias duradouras.</p>
+                <button class="bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 hover:border-yellow-500 rounded">
+                    Quero saber mais
+                </button>
+                <button class="bg-transparent hover:bg-yellow-500 text-yellow-700 font-semibold hover:text-white py-2 px-4 border border-yellow-500 hover:border-transparent rounded">
+                    Quero participar    
+                </button>
+                <p class="mt-3">Conheça os <span class="font-bold">últimos vencedores</span></p>
+            </div>
+        </div>
+    </div>
+    
+          
+       
+           <div class="">
+            <div class="xl:max-w-2xl">
+                <h2 class="text-4xl font-bold tracking-tight text-black sm:text-5xl"><span class="text-yellow-500">Concorra</span> uma experiência única com seus <span class="text-yellow-500">ídolos!</span></h2>
+            </div>
+        </div>
+              
+           
+       
+      
+                
+            
+        
+
+        
+</body>
+</html>
